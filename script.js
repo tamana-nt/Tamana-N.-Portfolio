06ae1d20b02d5694d0465ace16ac3fe8ef72a7c4
tamana-nt <tamanabnasrat@gmail.com>
2025-05-30 09:10:51 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    alert('Welcome to my portfolio!');
+};
+
+
+document.getElementById('about').onclick = function() {
+    document.getElementById('about').style.backgroundColor = '#e0e0e0';
+};
+
